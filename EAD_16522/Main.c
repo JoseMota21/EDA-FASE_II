fe50f73bd89e12d2d2cbf8e3ab5cf3db010d6cde
JoseMota21 <a16522@alunos.ipca.pt>
2023-03-11 11:41:21 +0000

    Reformulação de main 11/03

--- EAD_16522/Main.c
+++ EAD_16522/Main.c
@@ -3,134 +3,112 @@
 #include "transporte.h" 
 #include "Cliente.h" 
 #include <stdlib.h>
+#define true 1; 
+#define false 2; 
+
+typedef int bool;
 
 int main() {
 	//Inicializar as listas 
-	Transporte* meioTransporte_1 = NULL; // Lista ligada vazia  
-	Cliente* cliente_1 = NULL; // Lista ligada vazia 
+	Transporte* meioTransporte_1 = NULL; // Lista ligada transportes vazia  
+	Cliente* cliente_1 = NULL; // Lista ligada clientes vazia 
 	
-	//Variáveis alocadas ao swicth case 
-	int user;
+	int opcao; 
 	int gestor;
-	int eliminar; 
-	int consultar; 
-	int MenuCliente; 
+	int cliente;
 	
-	
+	bool logado = true; 
+
 	//Ler o ficheiro Txt
 	//Ficheiro Transportes
 	meioTransporte_1 = lerFicheiroTransporte(meioTransporte_1);
 	// Ficheiro txt do cliente
 	cliente_1 = lerFicheiroCliente(cliente_1); 
-	
-	printf("EAD 16522\n");
 
-	printf("1 - GESTOR\n");
-	printf("2 - CLIENTE\n");
-	scanf("%d", &user);
+	do {
+		//system("cls");
+		printf("M E N U\n");
+		printf("1 - GESTOR\n");
+		printf("2 - CLIENTE\n");
+		printf("0 - SAIR\n");
 
-	//Limpar consola 
-	system("cls");
+		scanf("%d", &opcao);
 
-	//Selecionar se é gestor ou cliente
-	switch (user) {
-		//Se gestor 
-	case 1:
-		printf("BEM VINDO GESTOR\n");
+		switch (opcao){
 
-		printf("1 - INSERIR TRANSPORTE \n");
-		printf("2 - INSERIR CLIENTE \n");
-		printf("3 - REMOVER DADOS \n");
-		printf("4 - CONSULTAR DADOS \n");
-
-		scanf("%d", &gestor);
-
-		//Limpar consola 
-		system("cls");
-
-		//Selecionar as operações do gestor
-		switch (gestor) {
-		case 1:
-			//Adicionar meios de transporte através da escrita no teclado
-			inputTransporte(meioTransporte_1);
-		break;
-
-		case 2: 
-			//Adicionar cliente através da escrita no teclado 
-			inputCliente(cliente_1); 
+		case 1: 
+			do {
+				printf("1 - INSERIR TRANSPORTE NOVO\n");
+				printf("2 - INSERIR CLIENTE NOVO\n");
+				printf("3 - REMOVER TRANSPORTE\n");
+				printf("4 - REMOVER CLIENTE\n");
+				printf("5 - ORDENAR TRANSPORTE AUTONOMIA\n");
+				printf("6 - CONSULTAR LISTAR DE TRANSPORTE\n"); 
+				printf("7 - CONSULTAR LISTA DE CLIENTES\n");
+				scanf("%d", &gestor);
+				system("cls");
+				switch (gestor){ 
+				case 1: 
+					inputTransporte(meioTransporte_1);
+					break;
+				case 2: 
+					inputCliente(cliente_1);
+					break; 
+				case 3: 
+					RemoverTransporte(meioTransporte_1);
+					break; 
+				case 4: 
+					RemoverCliente(cliente_1); 
+					break;
+				case 5: 
+					OrdenarTransportesPorAutonomiaDecrescente(meioTransporte_1);
+					break;
+				case 6: 
+					listarTransporte(meioTransporte_1);
+					break;
+				case 7: 
+					listarCliente(cliente_1); 
+					break;
+				default:
+					printf("OPCAO\n");
+					break;
+				}
+				
+			} while (gestor !=0);
 			break;
-		case 3: 
-			printf("1 - REMOVER TRANSPORTE\n"); 
-			printf("2 - REMOVER CLIENTE\n");
+		case 2:
+			do {
+				logado = loginCliente(cliente_1);
+				if (logado){
 
-			scanf("%d", &eliminar); 
-			
-			switch (eliminar) {
+					printf("1 - CONSULTAR DADOS MEUS DADOS\n");
+					printf("2 - ALTERAR MEUS DADOS\n");
+					scanf("%d", &cliente);
 
-			case 1: 
-				//Remover transporte
-				RemoverTransporte(meioTransporte_1); 
-				break; 
-
-			case 2: 
-				//Remover cliente
-				RemoverCliente(cliente_1); 
-				break; 
-	
-			default:
-				printf("OPCAO INVALIDA"); 
-				break;
-			}
+					switch (cliente) {
+					case 1:
+						clientedados(cliente_1);
+						break;
+					case 2:
+						AlterarDados(cliente);
+						break;
+					default:
+						printf("OPCAO \n");
+						break;
+					}
+					break;
+				}
+				
+			} while (logado !=0);
 			break;
-		case 4: 
-			printf("1 - CONSULTAR TRANSPORTE\n");
-			printf("2 - CONSULTAR CLIENTE\n");
-			scanf("%d", &consultar);
-			switch (consultar) {
-			case 1: 
-				//Ordenar Transporte por autonomia (decrescente)
-				OrdenarTransportesPorAutonomiaDecrescente(meioTransporte_1);
-				break; 
-
-			default:
-				printf("opcao invalida"); 
-				break;
-			}
-			break; 
-			break; 
+		default:
+			printf("OPCAO INVALIDA\n");
+			break;
 		}
 		break;
-	case 2: 
-		//Login com o user cliente (Acesso com o login e passwrod) 
-		cliente_1 = loginCliente(cliente_1); 
 
-		if (cliente_1 != NULL) {
-			printf("MENU CLIENTE\n");
-
-			printf("1 - CONSULTAR DADOS\n");
-			printf("2 - ALTERAR DADOS\n");
-			scanf("%d", &MenuCliente);
-
-			switch (MenuCliente) {
-
-			case 1:
-				//Cliente a consultar dados
-				clientedados(cliente_1);
-				break;
-			case 2: 
-				//Alterar dados de cliente 
-				AlterarDados(cliente_1);
-				break;
-
-			default:
-				printf("OPCAO INVALIDA\n");
-				break;
-			}
-		}
-
-
-	}
-
+	} while (opcao != 0);
+	
 	return;
 }
 	
