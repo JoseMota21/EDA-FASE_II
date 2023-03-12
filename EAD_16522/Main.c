2f879ebbcaeccefa3d22630732dc6477e8490e90
JoseMota21 <a16522@alunos.ipca.pt>
2023-03-12 11:23:20 +0000

    switch case a funcionar

--- EAD_16522/Main.c
+++ EAD_16522/Main.c
@@ -3,30 +3,31 @@
 #include "transporte.h" 
 #include "Cliente.h" 
 #include <stdlib.h>
-#define true 1; 
-#define false 2; 
+
 
 typedef int bool;
+#define true 1 
+#define false 0
 
 int main() {
 	//Inicializar as listas 
 	Transporte* meioTransporte_1 = NULL; // Lista ligada transportes vazia  
 	Cliente* cliente_1 = NULL; // Lista ligada clientes vazia 
-	
-	int opcao; 
+
+	int opcao;
 	int gestor;
 	int cliente;
-	
-	bool logado = true; 
+
+	bool logado = true;
 
 	//Ler o ficheiro Txt
 	//Ficheiro Transportes
 	meioTransporte_1 = lerFicheiroTransporte(meioTransporte_1);
 	// Ficheiro txt do cliente
-	cliente_1 = lerFicheiroCliente(cliente_1); 
+	cliente_1 = lerFicheiroCliente(cliente_1);
 
 	do {
-		//system("cls");
+		system("cls");
 		printf("M E N U\n");
 		printf("1 - GESTOR\n");
 		printf("2 - CLIENTE\n");
@@ -34,81 +35,88 @@ int main() {
 
 		scanf("%d", &opcao);
 
-		switch (opcao){
+		switch (opcao) {
 
-		case 1: 
+		case 1:
 			do {
 				printf("1 - INSERIR TRANSPORTE NOVO\n");
 				printf("2 - INSERIR CLIENTE NOVO\n");
 				printf("3 - REMOVER TRANSPORTE\n");
 				printf("4 - REMOVER CLIENTE\n");
 				printf("5 - ORDENAR TRANSPORTE AUTONOMIA\n");
-				printf("6 - CONSULTAR LISTAR DE TRANSPORTE\n"); 
+				printf("6 - CONSULTAR LISTAR DE TRANSPORTE\n");
 				printf("7 - CONSULTAR LISTA DE CLIENTES\n");
+				printf("8 - SAIR\n");
 				scanf("%d", &gestor);
 				system("cls");
-				switch (gestor){ 
-				case 1: 
+
+				switch (gestor) {
+				case 1:
+
 					inputTransporte(meioTransporte_1);
 					break;
-				case 2: 
+				case 2:
+
 					inputCliente(cliente_1);
-					break; 
-				case 3: 
-					RemoverTransporte(meioTransporte_1);
-					break; 
-				case 4: 
-					RemoverCliente(cliente_1); 
 					break;
-				case 5: 
+				case 3:
+
+					RemoverTransporte(meioTransporte_1);
+					break;
+				case 4:
+
+					RemoverCliente(cliente_1);
+					break;
+				case 5:
+
 					OrdenarTransportesPorAutonomiaDecrescente(meioTransporte_1);
 					break;
-				case 6: 
+				case 6:
+
 					listarTransporte(meioTransporte_1);
 					break;
-				case 7: 
-					listarCliente(cliente_1); 
+				case 7:
+
+					listarCliente(cliente_1);
 					break;
+				case 8: 
+					break; 
+
 				default:
-					printf("OPCAO\n");
+					printf("OPCAO INVALIDA\n");
 					break;
 				}
-				
-			} while (gestor !=0);
+
+			} while (gestor != 8);
 			break;
-		case 2:
+		
+		case 2: 
+			logado = loginCliente(cliente_1); 
 			do {
-				logado = loginCliente(cliente_1);
-				if (logado){
+				printf("1 - CONSULTAR DADOS MEUS DADOS\n");
+				printf("2 - ALTERAR MEUS DADOS\n");
+				printf("3 - SAIR\n");
+				scanf("%d", &cliente);
 
-					printf("1 - CONSULTAR DADOS MEUS DADOS\n");
-					printf("2 - ALTERAR MEUS DADOS\n");
-					scanf("%d", &cliente);
-
-					switch (cliente) {
-					case 1:
-						clientedados(cliente_1);
-						break;
-					case 2:
-						AlterarDados(cliente);
-						break;
-					default:
-						printf("OPCAO \n");
-						break;
-					}
+				switch (cliente){ 
+				case 1: 
+					clientedados(cliente_1); 
+					break;
+				case 2: 
+					AlterarDados(cliente_1); 
+					break; 
+				case 3: 
+					break; 
+				default:
 					break;
 				}
-				
-			} while (logado !=0);
-			break;
-		default:
-			printf("OPCAO INVALIDA\n");
-			break;
-		}
-		break;
 
-	} while (opcao != 0);
-	
+			} while (cliente != 3);
+			
+		}
+		
+	} while (opcao != 0); 
+		
 	return;
 }
 	
