68f30b008531f2adfec81078d5ea02e084274132
JoseMota21 <a16522@alunos.ipca.pt>
2023-03-18 12:47:04 +0000

    Aluguer.c e .h organizado

--- EAD_16522/Aluguer.c
+++ EAD_16522/Aluguer.c
@@ -6,15 +6,18 @@
 //Alugar Veiculo
 void alugarTranporte(Cliente* cliente_1, Transporte* meioTransporte_1, int nif) {
 
+	//Variáveis 
 	float distancia = 0.0;
 	float preco = 0.0;
 
-	system("cls"); 
+	system("cls"); // Limpar consola
 
-	// Encontrar o cliente com o NIF indicado
+	//Inicio da lista
 	Cliente* atualC = cliente_1;
+
+	// Procura na lista o cliente com o NIF logado na aplicação 
 	while (atualC != NULL && atualC->NIF != nif) {
-		atualC = atualC->seguinte;
+		  atualC = atualC->seguinte;
 	}
 
 	//Se o cliente já tiver veiculo alugago impossivel alugar transporte
@@ -24,6 +27,7 @@ void alugarTranporte(Cliente* cliente_1, Transporte* meioTransporte_1, int nif) 
 		return;
 	}
 
+	//Pedir ao utilizador para inserir a distancia que vai percorrer
 	printf("DISTANCIA A PERCORRER\n");
 	scanf("%f", &distancia);
 
@@ -51,37 +55,38 @@ void alugarTranporte(Cliente* cliente_1, Transporte* meioTransporte_1, int nif) 
 	printf("INSIRA O ID DO TRANSPORTE DESEJADO:\n");
 	scanf("%d", &ID);
 
-	// Procurar o transporte na lista // DISPONIVEL != 0
+	//Inicio da lista
 	Transporte* atual = meioTransporte_1;
 
 	//Calculo da bateria necessária 
 	float bateria;
 	bateria = (atual->bateria * distancia) / 80;
 
+	//Procura na lista de transportes o ID indicado pelo o cliente 
 	while (atual != NULL && atual->codigo != ID) {
 		atual = atual->seguinte;
 	}
-	if (atual == NULL) {
-		// Transporte não encontrado
+	if (atual == NULL) { // Transporte não encontrado 
+		
 		printf("TRANSPORTE COM O ID %d NAO ENCONTRADO\n", ID);
 
 		system("pause"); 
 		system("cls");
 	}
-	else if (atual->disponivel == 0) {
-		//Transporte indisponível
+	else if (atual->disponivel == 0) { //Transporte indisponível 
+		
 		printf("TRANSPORTE %d INDISPONIVEL\n", ID);
 		system("pause");
 		system("cls");
 	}
-	else if (atual->autonomia < distancia) {
+	else if (atual->autonomia < distancia) { //Autonomia insuficiente
 
 		printf("AUTONOMIA INSUFICENTE PARA TERMINAR A VIAGEM\n");
 		system("pause");
 		system("cls");
 
-	}//Calcular se tem bateria suficiente (embora não faça sentido uma vez que a autonomia está calculada em propoção com a bateria)
-	else if (atual->bateria < bateria) {
+	}
+	else if (atual->bateria < bateria) { //Bateria insuficiente
 
 		printf("BATERIA INSUFICENTE PARA TERMINAR A VIAGEM\n");
 		system("pause");
@@ -95,10 +100,11 @@ void alugarTranporte(Cliente* cliente_1, Transporte* meioTransporte_1, int nif) 
 		printf("\n");
 		printf("| %-5s | %-10s | %-8s | %-10s | %-30s |\n", "ID", "TIPO", "BATERIA", "AUTONOMIA", "LOCALIZACAO");
 		printf("|-------|------------|----------|------------|--------------------------------|\n");
+
+		//Mostra na consola o transporte que foi selecionado e que está disponivel 
 		printf("| %-5d | %-10s | %-8.2f | %-10.2f | %-30s |\n", atual->codigo, atual->tipo, atual->bateria, atual->autonomia, atual->geocodigo);
 
-		//printf("%d; %s; %.2f; %.2f; %s\n", atual->codigo, atual->tipo, atual->bateria, atual->autonomia, atual->geocodigo);
-		atual->disponivel = 0;
+		atual->disponivel = 0; // Veiculo alugado fica indisponivel para alugar
 
 		//Escrever o ID do veiculo alugado na estrutura cliente no campo IDVEIVULOALUGADO
 		atualC->IDveiculoAlugado = atual->codigo;
@@ -106,7 +112,7 @@ void alugarTranporte(Cliente* cliente_1, Transporte* meioTransporte_1, int nif) 
 		//Subtrair o saldo do cliente ao preço (euro) da viagem
 		atualC->saldo -= preco;
 
-		//Descontar a autonomia à estrutura do veiculo (km)
+		//Desconta a autonomia à estrutura do veiculo (km)
 		atual->autonomia -= distancia;
 
 		//Descontar a percentagem da bateria
@@ -119,9 +125,9 @@ void alugarTranporte(Cliente* cliente_1, Transporte* meioTransporte_1, int nif) 
 		system("cls");
 	}
 
-	//Atualizar o ficheiro txt 
+	//Atualizar o ficheiro cliente 
 	saveAlterarDados(cliente_1);
-	//Atualizar o ficheito txt 
+	//Atualizar o ficheito transporte 
 	saveAlterarDadosTransportes(meioTransporte_1);
 
 }
@@ -129,40 +135,47 @@ void alugarTranporte(Cliente* cliente_1, Transporte* meioTransporte_1, int nif) 
 //Desalugar veiculo 
 void desalugarVeiculo(Cliente* cliente_1, Transporte* meioTransporte_1, int nif) {
 
-	// Encontrar o cliente com o NIF indicado
+	//Inicio da lista
 	Cliente* atualC = cliente_1;
+
+	// Procura na lista o cliente com o NIF logado na aplicação 
 	while (atualC != NULL && atualC->NIF != nif) {
 		atualC = atualC->seguinte;
 	}
 
-	//Se não tiver veiculo alugado impossivel desalugar
+	//Se não tiver veiculo alugado impossivel desalugar, informa o utilizador 
 	if (atualC->IDveiculoAlugado == -1) {
 
 		printf("SEM VEICULO ALUGADO\n"); 
 		return; 
 	}
 	
+	//Inicio da lista 
 	Transporte* atual = meioTransporte_1; 
 
-	//Percorrer a lista 
+	//Percorrer a lista dos transportes até encontrar o nó que contém o veiculo alugado pelo o cliente
+	//Verifica se não há mais para percorrer e verifica se o codigo do veiculo na estrutura transporte 
+	//não é igual ao código do veiculo alugado
 	while (atual!= NULL && atual->codigo != atualC->IDveiculoAlugado){ 
 		atual = atual->seguinte; 
 	}
 
-	if (atual == NULL) {
+	if (atual == NULL) { //Não encontrou o codigo do veiculo alugado 
 		printf("TRANSPORTE NÃO ENCONTRADO\n"); 
 	}
 	else {
-		//Transporte está disponivel
+		//Transporte fica disponivel
 		atual->disponivel=1; 
 
-		//Limpar o campo IDVEICULOALUGADO
+		//Cliente não tem veiculo alugado
 		atualC->IDveiculoAlugado = -1; 
 
 		printf("VEICULO ENTREGUE\n"); 
 	}
 
+	//Guardar dos dados no ficheiro cliente
 	saveAlterarDados(cliente_1); 
+	//Guarda os dados no ficheiro transportes
 	saveAlterarDadosTransportes(meioTransporte_1); 
 
 	system("pause");
@@ -172,15 +185,17 @@ void desalugarVeiculo(Cliente* cliente_1, Transporte* meioTransporte_1, int nif)
 //Historico de aluguer 
 void historico(Cliente* cliente_1, Transporte* meioTransporte_1) {
 
-	//a para não alterar o historio, irá escrever sempre no fim
-	FILE* historico = fopen("Historico.txt", "ab"); 
+	//Abre o ficheiro 
+	FILE* historico = fopen("Historico.txt", "a"); 
+
+	//Se ficheiro não aberto corretamente informa o utilizador
 	if (historico == NULL) {
-		printf("Erro ao abrir o arquivo de histórico\n");
+		printf("ERRO AO ABRIR O HISTORICO\n");
 		return;
 	}
 
+	//Escreve no ficheiro txt 
 	fprintf(historico,"%s,%d,%d,%s\n", cliente_1->nome_cliente, cliente_1->NIF, meioTransporte_1->codigo, meioTransporte_1->tipo);
-
 
 	fclose(historico);
 }
@@ -191,28 +206,32 @@ void* consultarHistorico() {
 	//Abrir o ficheiro com os dados de historico
 	FILE* ficheiroHistorico = fopen("Historico.txt", "r");
 
+	//Se ficheiro não aberto corretamente informa o utilizador
 	if (ficheiroHistorico == NULL) {
 		printf("ERRO AO ABRIR O HISTORICO\n");
 		return;
 	}
 
-	char linha[100]; 
+	//Variavel
+	char linha[1000]; 
+	char nome_cliente[50]; 
+	char tipo[20];
+	int nif, codigo;
 
 	printf("\t++++++++++++++++++++++++++++++++++++++++++ CONSULTAR HISTORICO +++++++++++++++++++++++++++++++++++++++\n");
 	printf("\n");
 	printf("| %-30s | %-20s | %-10s | %-20s |\n", "NOME", "NIF", "ID", "TIPO");
 	printf("|--------------------------------|----------------------|------------|----------------------|\n");
 
-	while (fgets(linha, sizeof(linha), ficheiroHistorico)) {
-		char nome_cliente[50], tipo[20];
-		int nif, codigo;
+	//Lê as informações contidas no ficheiro
+	while (fgets(linha, sizeof(linha), ficheiroHistorico)) { //Lê cada linha do ficheiro e armazena na variável linha
+		
 		sscanf(linha,"%[^,],%d,%d,%[^,\n]", nome_cliente, &nif, &codigo, tipo);
 
+		//Imprime na consola 
 		printf ("| %-30s | %-20d | %-10d | %-20s |\n", nome_cliente, nif, codigo, tipo);
 		
-
-		//printf("%s; %d; %d; %s\n", nome_cliente, nif, codigo, tipo);
 	}
-
+	//Fecha o ficheiro 
 	fclose(ficheiroHistorico);
 }
