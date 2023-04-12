e0210cbd899ce51c058165b08287e477159b0885
JoseMota21 <a16522@alunos.ipca.pt>
2023-04-13 00:03:28 +0100

    FASE II

--- EAD_16522/Aluguer.h
+++ EAD_16522/Aluguer.h
@@ -11,3 +11,6 @@ void alugarTranporte(Cliente* cliente_1, Transporte* meioTransporte_1, int nif);
 //Entregar o veiculo alugado
 void desalugarVeiculo(Cliente* cliente_1, Transporte* meioTransporte_1); 
 
+
+
+
