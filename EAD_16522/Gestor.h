481ff45277f65652f89530ccee575355608b9754
JoseMota21 <a16522@alunos.ipca.pt>
2023-05-28 23:32:48 +0100

    Gestor dozygen

--- EAD_16522/Gestor.h
+++ EAD_16522/Gestor.h
@@ -1,38 +1,85 @@
 #include <stdio.h>
 #include <stdbool.h> 
 
-//Estrutura Gestor
+/**
+ * @brief Estrutura de dados para representar um registo de gestor.
+ * A estrutura `Gestor` armazena informações sobre um gestor, incluindo nome, email e senha.
+ * Também possui um apontador para o próximo gestor na lista.
+ */
 typedef struct registoGestor {
 
-	char nome[100];
-	char email[100]; 
-	char password[100]; 
+	char nome[100]; ///<Nome do gestor 
+	char email[100]; ///<Email do gestor
+	char password[100]; ///<senha do gestor 
 
-	struct resgistoGestor* seguinte;  //Apontador para o proximo gestor da lista
+	struct resgistoGestor* seguinte;  ///< Apontador para o proximo gestor da lista
 
 }Gestor;
 
-//Estrutura boleana de login (Informa se sucesso/insucesso de login e o gestor) 
+/** 
+ * @brief Estrutura de dados para representar o estado de login de um gestor.
+ * A estrutura `loginGestor` armazena informações sobre o estado de login de um gestor,
+ * incluindo se está autenticado e um apontador para o gestor autenticado.
+ */
 typedef struct {
-	bool autenticado;
-	Gestor* gestor;
+	bool autenticado; ///<Gestor logado 
+	Gestor* gestor; ///<Apontador para o gestor autenticado
 } loginGestor;
 
-//Guardar gestor em ficheiro txt 
+/**
+ * @brief Insere um novo gestor na lista de gestores.
+ * A função `inserirGestor` cria uma nova instância da estrutura `Gestor`,
+ * solicita informações ao gestor para preencher os dados do novo gestor e
+ * adiciona-o no início da lista de gestores.
+ *
+ * @param gestor_1 Apontador para o início da lista de gestores.
+ * @return O apontador para o novo gestor inserido.
+ */
+Gestor* inserirGestor(Gestor* gestor_1);
+
+/**
+ * @brief Verifica se já existe um gestor com o email inserido.
+ * A função `existeGestor` percorre a lista de gestores a partir do gestor
+ * passado como parâmetro e verifica se existe algum gestor com o email
+ * fornecido. Retorna o apontador para o gestor encontrado ou NULL caso não
+ * exista.
+ * @param gestor_1 Apontador para o início da lista de gestores.
+ * @param email Email a ser verificado.
+ * @return O apontador para o gestor encontrado ou NULL caso não exista.
+ */
+Gestor* existeGestor(Gestor* gestor_1, const char* email); 
+ 
+/**
+ * @brief Salva os dados dos gestores em um arquivo de texto.
+ * A função `saveficheiroGestor` percorre a lista de gestores a partir do gestor
+ * passado como parâmetro e escreve os dados de cada gestor em um arquivo de texto.
+ * Os dados são separados por ponto e vírgula (;). Retorna o apontador para o início
+ * da lista de gestores.
+ * @param inicio Apontador para o início da lista de gestores.
+ * @return O apontador para o início da lista de gestores.
+ */
 Gestor* saveficheiroGestor(Gestor* inicio);
 
-//Inserir novo gestor no inicio da lista
-Gestor* inserirGestor(Gestor* gestor_1); 
-
-//Guardar gestor em ficheiro txt 
-Gestor* saveficheiroGestor(Gestor* inicio); 
-
-//Ler o ficheiro do gestor txt 
+/**
+ * @brief Lê os dados dos gestores a partir de um arquivo de texto.
+ * A função `lerficheiroGestor` abre o arquivo de texto "Gestor.txt" e percorre cada linha,
+ * armazenando os dados em uma estrutura de gestor. Os dados são lidos na seguinte ordem:
+ * nome, email e senha. Em seguida, o novo gestor é adicionado ao final da lista de gestores.
+ * Retorna o apontador para o início da lista de gestores.
+ * @param inicio Apontador para o início da lista de gestores.
+ * @return O apontador para o início da lista de gestores.
+ */
 Gestor* lerficheiroGestor(Gestor* inicio); 
 
-//Login de Gestor
+/**
+ * @brief Realiza o login de um gestor.
+ * A função `gestorlogin` solicita ao gestor que insira seu email e senha. Em seguida, ela percorre a lista de gestores
+ * para encontrar um gestor que corresponda ao email e senha fornecidos. Se encontrar um gestor correspondente, define o
+ * campo `autenticado` como verdadeiro e armazena as informações do gestor no campo `gestor` da estrutura `loginGestor`.
+ * Caso contrário, define o campo `autenticado` como falso. Retorna a estrutura `loginGestor` contendo as informações
+ * sobre a autenticação do gestor.
+ * @param Login Apontador para o início da lista de gestores.
+ * @return A estrutura `loginGestor` contendo as informações sobre a autenticação do gestor.
+ */
 loginGestor gestorlogin(Gestor* Login); 
-
-//Verificar se já existe o gestor por email 
-Gestor* existeGestor(Gestor* gestor_1, const char* email);
 
--- EAD_16522/Historico.h
+++ EAD_16522/Historico.h
@@ -6,7 +6,10 @@
 #ifndef HISTORICO_H
 #define HISTORICO_H 
 
-//Estrutura para historico
+/**
+ * @brief Estrutura para o registo de histórico. 
+ * Esta estrutura representa um registo de histórico, contem informações sobre a viagem (deslocação) que foi realizada. 
+ */
 typedef struct historico {
  
 	char nome_cliente[80]; 
@@ -21,21 +24,59 @@ typedef struct historico {
 
 }HistoricoRegisto; 
 
-extern HistoricoRegisto* historico; 
+/**
+ * @brief Apontador externo para a lista de histórico.
+ * Variável externa que armazena o apontador para a lista de histórico de registo.
+ * Essa variável pode ser utilizado em outros ficheiro para referenciar a lista de histórico. 
+ */
+extern HistoricoRegisto* historico;  
 
-//Inserir novo registo à estrutura do historico 
-void InserirRegisto(HistoricoRegisto* historico_1, char* nome, int NIF, char* tipo, int id, float preco, float distanciaPer, char* localidadeIni, char* localidadeFim);
+/**
+ * @brief Insere um novo registo na lista de histórico.
+ *
+ * Esta função cria um novo registo de histórico e o insere no final da lista.
+ *
+ * @param historico Um apontador para o apontador da lista de histórico.
+ * @param nome O nome do cliente.
+ * @param NIF O NIF do cliente.
+ * @param tipo O tipo de viagem.
+ * @param id O ID do registro.
+ * @param preco O preço da viagem.
+ * @param distanciaPer A distância percorrida na viagem.
+ * @param localidadeIni A morada inicial da viagem.
+ * @param localidadeFim A morada final da viagem.
+ */
+void InserirRegisto(HistoricoRegisto** historico_1, char* nome, int NIF, char* tipo, int id, float preco, float distanciaPer, char* localidadeIni, char* localidadeFim);
 
-//Guardar a estrutura historico em ficheiro txt
+/**
+ * @brief Guarda o histórico em ficheiro.
+ * Esta função percorre a lista de histórico e escreve cada registo no ficheiro de texto.
+ * @param historico Um apontador para a lista de histórico.
+ */
 void GuardarHistorico(HistoricoRegisto* historico); 
 
-//Consultar o historico 
+/**
+ * @brief Consulta o histórico na consola do Gestor.
+ * Esta função exibe os registo de todo histórico na consola.
+ * @param historico Um apontador para a lista de histórico.
+ * @return Um ponteiro nulo.
+ */
 HistoricoRegisto* consultarhistorico (HistoricoRegisto* historico); 
 
-//Ler ficheiro TXT 
+/**
+ * @brief Lê o ficheiro de texto do histórico.
+ * Esta função lê o ficheiro de texto "historico.txt" e armazena os registo na lista de histórico.
+ * @param historico Um apontador para a lista de histórico.
+ * @return Um apontador para a lista de histórico atualizada.
+ */
 HistoricoRegisto* lerficheirohistorico(HistoricoRegisto* historico);
 
-//Cliente consulta historico 
+/**
+ * @brief Consulta o histórico de um cliente específico (logado).
+ * Esta função exibe na tela o histórico de viagens de um cliente com base no seu NIF. 
+ * @param historico Um apontador para a lista de histórico.
+ * @param nif O NIF do cliente para o qual o histórico será consultado.
+ */
 void consultarHistoricoCliente(HistoricoRegisto* historico, int nif); 
 
 #endif "HISTORICO_H" 
